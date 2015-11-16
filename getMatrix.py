f4374735cf48b1a82f93e6bb1638accd29080912
John Du <du55@purdue.edu>
2015-11-16 17:26:22 -0500

    Add more matrix things to getMatrix.py

--- getMatrix.py
+++ getMatrix.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
+import random;
+import numpy;
 import sys;
 
 #args: String filename
 #return: 2d int array adjacency matrix
-def getMatrix(filename):
+def getMatrixFromFile(filename):
     fd = open(filename, 'r');
     rows = fd.read().split("\n");
     fd.close();
@@ -15,10 +17,46 @@ def getMatrix(filename):
             nodes[i][j] = int((rows[i].split(" "))[j]);
     return nodes;
 
+def getIsomorphismDefinition(n):
+    definition = list(range(n));
+    random.shuffle(definition);
+    return definition;
+
+def getIsomorphismDefinitionMatrix(definition):
+    n = len(definition);
+    nodes = [[0 for i in range(n)] for i in range(n)];
+    for i in range(n):
+        nodes[i][definition[i]] = 1;
+    return nodes;
+
+def getIsomorphism(matrix, isomorphism):
+    pi = numpy.matrix(isomorphism);
+    pit = numpy.transpose(pi);
+    m = numpy.matrix(matrix);
+    return pi * m * pit;
+
 def getMatrixTest():
     filename = sys.argv[1];
-    matrix = getMatrix(filename);
+    matrix = getMatrixFromFile(filename);
     print matrix;
-    print matrix[0][0] + matrix[0][1];
 
+def getIsomorphismTest():
+    n = 4;
+    definition = getIsomorphismDefinition(n);
+    pi = getIsomorphismDefinitionMatrix(definition);
+    matrix = numpy.random.randint(2, size=(n, n));
+    for i in range(n):
+        matrix[i][i] = 0;
+        for j in range(n):
+            matrix[i][j] = matrix[j][i];
+    isomorphism = getIsomorphism(matrix, pi);
+    print matrix;
+    print definition;
+    print pi;
+    print isomorphism;
+
+print "Matrix tests";
 getMatrixTest();
+print "Isomorphism tests";
+getIsomorphismTest();
+
