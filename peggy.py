37017ae2fe1fa465dd8245ebfbe5c6b98962095d
park580 <park580@purdue.edu>
2015-11-18 15:27:33 -0500

    Wait, now i've removed mulity threading

--- peggy.py
+++ peggy.py
@@ -52,16 +52,16 @@ print 'Socket bind complete'
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
- 
-#Function for handling connections. This will be used to create threads
-def clientthread(conn):
-    #Sending message to connected client
-    conn.send('Thank you for connecting. Bye.')
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
     #conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
      
     #infinite loop so that function do not terminate and thread do not end.
     while True:
-         
         #Receiving from client
         data = conn.recv(1024)
         reply = 'OK...' + data
@@ -72,14 +72,5 @@ def clientthread(conn):
      
     #came out of loop
     conn.close()
- 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
  
 s.close()
