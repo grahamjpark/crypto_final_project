4eb85021159353a8cb7037f12d68a8a8d78abc3b
park580 <park580@purdue.edu>
2015-11-18 22:45:21 -0500

    Abstracted flipCoin to its own file.

---
+++ flipCoin.py
@@ -0,0 +1,60 @@
+import hashlib
+import random
+import sys
+import os
+import math
+import binascii
+import socket
+import sys
+from thread import *
+
+__rand = ""
+__hash = ""
+__bit = 0
+__flipped = 0
+
+def flipCoin():
+	#Uses OS to generate 124 bit random, then converts it to hexadecimal string
+	rand = binascii.hexlify(os.urandom(124))
+	# print '~~~~~~~Random Number~~~~~~~'
+	# print rand
+	global __rand
+	__rand = rand
+
+	# print '~~~~~~~Hash of Random~~~~~~~'
+	#Sets up the hash function
+	m = hashlib.md5();
+	#Adds the random hex string to be hashed
+	m.update(rand)
+	#Hashes the string (which outputs stuff) and converts that to a hex string
+	global __hash
+	__hash = binascii.hexlify(m.digest());
+	# print __hash
+
+	# print '~~~~~~~First Char of Hash and Commitment~~~~~~~'
+	#Selects the first char in that random hex string
+	# print rand[0:1]
+	#Converts that char to an int and takes it mod 2 to get the bit commitment
+	__bit = ord(rand[0:1]) % 2
+	# print __bit
+	global __flipped
+	flipped = 1
+	print 'Coin fliped. Variables set'
+
+def getRandom():
+	if __flipped:
+		return __rand
+	else:
+		return -1
+
+def getHash():
+	if __flipped:
+		return __hash
+	else:
+		return -1
+
+def getBit():
+	if __flipped:
+		return __bit
+	else:
+		return -1
--- peggy.py
+++ peggy.py
@@ -10,32 +10,10 @@ import binascii
 import socket
 import sys
 from thread import *
+from flipCoin import *
 
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 12346 # Arbitrary non-privileged port
-
-def flipCoin(connection):
-	#Uses OS to generate 124 bit random, then converts it to hexadecimal string
-	rand = binascii.hexlify(os.urandom(124))
-	print '~~~~~~~Random Number~~~~~~~'
-	print rand
-
-	print '~~~~~~~Hash of Random~~~~~~~'
-	#Sets up the hash function
-	m = hashlib.md5();
-	#Adds the random hex string to be hashed
-	m.update(rand)
-	#Hashes the string (which outputs stuff) and converts that to a hex string
-	print binascii.hexlify(m.digest());
-
-
-	print '~~~~~~~First Char of Hash and Commitment~~~~~~~'
-	#Selects the first char in that random hex string
-	print rand[0:1]
-	#Converts that char to an int and takes it mod 2 to get the bit commitment
-	print ord(rand[0:1]) % 2
-
-
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
